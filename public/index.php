33986cc1cd1c02a8fa7501b1eee7a2c0adf34e35
gituser <123@163.com>
2017-07-11 22:33:06 +0800

    index11122

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
-echo 11111;die();
+
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
  * @package  Laravel
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
+echo 11111;
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
